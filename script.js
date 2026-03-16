6ce78ded61cb1facab1f6235d87ae46b654bea73
stevenscott85 <stevenscott@hotmail.co.uk>
2026-03-16 08:56:36 +0000

    Create script.js

---
+++ script.js
@@ -0,0 +1,37 @@
+function startPlanner() {
+
+const services = [
+"DVLA",
+"Bank Accounts",
+"HMRC",
+"Council Tax",
+"Electoral Roll",
+"GP / NHS",
+"Insurance Policies",
+"Energy Supplier",
+"Broadband Provider",
+"Amazon",
+"PayPal",
+"Subscriptions",
+"Employer",
+"Mobile Phone Provider"
+];
+
+const planner = document.getElementById("planner");
+const checklist = document.getElementById("checklist");
+
+planner.classList.remove("hidden");
+
+checklist.innerHTML = "";
+
+services.forEach(service => {
+
+const li = document.createElement("li");
+
+li.innerHTML = `<input type="checkbox"> ${service}`;
+
+checklist.appendChild(li);
+
+});
+
+}
